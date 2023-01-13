11b0dc9c53b727ece9eddcd03769dbbe05e99370
Robert Evans <robermar2@gmail.com>
2023-01-13 12:02:39 -0500

    test template

---
+++ test/test_template.py
@@ -0,0 +1,15 @@
+import unittest
+from unittest.mock import patch, Mock
+
+
+
+class TestHandler(unittest.TestCase):
+
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def test_something(self):
+
+        self.assertEqual(1, 1)
+
+    

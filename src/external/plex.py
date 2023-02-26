73d2b4da0dae802a88057f4edbd5914987a26461
Robert Evans <robermar2@gmail.com>
2023-02-26 17:41:23 -0500

    plex post includes link to recently added media

--- src/external/plex.py
+++ src/external/plex.py
@@ -1,4 +1,5 @@
 import logging
+import urllib.parse
 
 from datetime import datetime, timedelta
 from plexapi.server import PlexServer
@@ -12,9 +13,10 @@ class PlexInstance:
     Interact with a plex server
     """
 
-    def __init__(self, plex_host: str, plex_token: str):
+    def __init__(self, plex_host: str, plex_token: str, plex_server_id: str):
         self.plex_host = plex_host
         self.plex_token = plex_token
+        self.plex_server_id = plex_server_id
 
     def get_recently_added(self, hours_since: int):
 
@@ -29,11 +31,15 @@ class PlexInstance:
                 now = datetime.now()
                 hours_age = (now - item_added_at) / timedelta(hours=1)
                 if hours_age <= hours_since:
-                    item_to_add = PlexRecentlyAddedItem(media_item=media_item)
+                    item_to_add = PlexRecentlyAddedItem(
+                        media_item=media_item, server_id=self.plex_server_id
+                    )
                     result.append(item_to_add)
                     logging.debug(f"Added to result: {media_item.title}")
                 else:
-                    logging.debug(f"Not added: {media_item.title}, added {hours_age} hours ago")
+                    logging.debug(
+                        f"Not added: {media_item.title}, added {hours_age} hours ago"
+                    )
 
         except Exception as e:
             logging.error(error_info(e))
@@ -46,15 +52,16 @@ class PlexRecentlyAddedItem:
     Summary info and poster image bytes for a plex recently added item
     """
 
-    def __init__(self, media_item):
+    def __init__(self, media_item, server_id: str):
         self.media_item = media_item
+        self.server_id = server_id
 
     def get_description(self):
         desc: str = ""
         if self.media_item.type == "season":
-            desc = f"A new episode was added to Plex for {self.media_item.parentTitle}!"
+            desc = f"A new episode was added to Plex for {self.media_item.parentTitle}!\n\n{self.get_public_uri()}"
         else:
-            desc = f"A {self.media_item.type} called {self.media_item.title} was added to Plex!.\n\nSummary:\n{self.media_item.summary}"
+            desc = f"A {self.media_item.type} called {self.media_item.title} was added to Plex!.\n\nSummary:\n{self.media_item.summary}\n\n{self.get_public_uri()}"
         return desc
 
     def get_poster_image_data(self):
@@ -62,3 +69,6 @@ class PlexRecentlyAddedItem:
 
     def get_post_image_file_name(self):
         return f"{self.media_item.title.replace(' ', '_')}.png"
+
+    def get_public_uri(self):
+        return f"https://app.plex.tv/desktop/#!/server/{self.server_id}/details?key={urllib.parse.quote(self.media_item.key, safe='')}&context=home%3Ahub.movie.recentlyadded~1~0"
